abfbec19e226bedd75851e2ee7b0de9a88abc3ba
dkarkuvelraja <karkuvelraja@gmail.com>
2024-12-13 01:23:52 +0530

    signup field added - praveen

--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -5,24 +5,36 @@ import { validation } from '../HelperFunctions/validation.ts';
 import { isValid } from '../HelperFunctions/basicHelpers.ts';
 
 interface IFormData {
+  firstName : string
+  lastName : string
   email: string;
+  gender: string
+  age: number
   password: string;
   phone_number: string;
 }
 
 function SignUp() {
   const [formData, setFormData] = useState<IFormData>({
+    firstName : "",
+    lastName : "",
     email: "",
+    gender: "",
+    age: 0,
     password: "",
     phone_number: "",
   });
   const [errors, setErrors] = useState<IFormData>({
+    firstName : "",
+    lastName : "",
     email: "",
+    gender: "",
+    age: 0,
     password: "",
     phone_number: "",
   });
   const [submitForm, { data, loading, error }] = useMutation(USER_RGN);
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleChange(e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
     const { name, value } = e.target;
 
     setFormData((prevData) => ({
@@ -35,10 +47,11 @@ function SignUp() {
     }))
   }
 
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement >) {
     e.preventDefault(); // Prevent page reload
     console.log("Form submitted:", formData);
     const errors = validation("signUp", formData)
+    setErrors(errors)
     console.log("errrors", errors)
     if (isValid(errors)) {
       console.log("Object.keys(errors).length > 0", Object.keys(errors))
@@ -47,9 +60,13 @@ function SignUp() {
         const submitData = await submitForm({ variables: { data: formData } });
         console.log("submitData", submitData)
         setFormData({
-          email: "",
-          password: "",
-          phone_number: "",
+          firstName : "",
+    lastName : "",
+    email: "",
+    gender: "",
+    age: 0,
+    password: "",
+    phone_number: "",
         })
       } catch (e) {
         console.error(e)
@@ -60,6 +77,28 @@ function SignUp() {
 
   return (
     <form onSubmit={handleSubmit}>
+    <div>
+        <label>First Name:</label>
+        <input
+          type="text"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+          placeholder="firstName"
+          />
+        {errors.firstName && <p style={{ color: "red" }}>{errors.firstName}</p>}
+      </div>
+      <div>
+        <label>Last Name:</label>
+        <input
+          type="text"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+          placeholder="last Name"
+          />
+        {errors.lastName && <p style={{ color: "red" }}>{errors.lastName}</p>}
+      </div>
       <div>
         <label>Email:</label>
         <input
@@ -68,21 +107,8 @@ function SignUp() {
           value={formData.email}
           onChange={handleChange}
           placeholder="Email"
-          required
         />
         {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
-      </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          placeholder="Password"
-          required
-        />
-        {errors.password && <p style={{ color: "red" }}>{errors.password}</p>}
       </div>
       <div>
         <label>Phone Number:</label>
@@ -92,9 +118,40 @@ function SignUp() {
           value={formData.phone_number}
           onChange={handleChange}
           placeholder="Phone Number"
-          required
         />
         {errors.phone_number && <p style={{ color: "red" }}>{errors.phone_number}</p>}
+      </div>
+      <div>
+        <label>Gender:</label>
+        <select name='gender' value={formData.gender} onChange={handleChange}>
+        <option value={""} disabled>Gender</option>
+          <option value={"male"}>Male</option>
+          <option value={"female"}>Female</option>
+          <option value={"others"}>Others</option>
+        </select>
+        {errors.gender && <p style={{ color: "red" }}>{errors.gender}</p>}
+      </div>
+      <div>
+        <label>Age:</label>
+        <input
+          type="number"
+          name="age"
+          value={formData.age}
+          onChange={handleChange}
+          placeholder="Age"
+        />
+        {errors.age && <p style={{ color: "red" }}>{errors.age}</p>}
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="Password"
+        />
+        {errors.password && <p style={{ color: "red" }}>{errors.password}</p>}
       </div>
       <button type="submit">Submit</button>
     </form>
